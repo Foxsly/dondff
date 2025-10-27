3e81aa3dc8135ce74f893e2f3ec99996c1999716
Foxsly <timothylbelcher@gmail.com>
2025-10-26 23:58:11 -0400

    Docker containerization - add health controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,13 @@
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { DatabaseModule } from '@/infrastructure/database/database.module';
+import { LeaguesModule } from '@/leagues/leagues.module';
+import { SleeperModule } from '@/sleeper/sleeper.module';
+import { TeamsModule } from '@/teams/teams.module';
+import { UsersModule } from '@/users/users.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { LeaguesModule } from './leagues/leagues.module';
-import { TeamsModule } from './teams/teams.module';
+import { HealthController } from '@/health/health.controller';
 import { ConfigModule } from '@nestjs/config';
-import { SleeperModule } from './sleeper/sleeper.module';
-import { DatabaseModule } from './infrastructure/database/database.module';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { DatabaseModule } from './infrastructure/database/database.module';
     TeamsModule,
     SleeperModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
