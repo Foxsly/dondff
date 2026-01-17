d2ee3f87a7ab5117a7ed14d837704f0da85eba6f
Foxsly <timothylbelcher@gmail.com>
2026-01-16 22:05:40 -0500

    Return some stats for fanduel projections

--- backend/src/fanduel/fanduel.controller.ts
+++ backend/src/fanduel/fanduel.controller.ts
@@ -1,3 +1,4 @@
+import { FanduelProjectionsResponse, PlayerPosition } from '@/fanduel/entities/fanduel.entity';
 import { Controller } from '@nestjs/common';
 import { FanduelService } from './fanduel.service';
 import { TypedRoute } from '@nestia/core';
@@ -7,7 +8,32 @@ export class FanduelController {
   constructor(private readonly fanduelService: FanduelService) {}
 
   @TypedRoute.Get('test')
-  getState() {
-    return this.fanduelService.getFanduelProjections();
+  async getState() {
+    const fP:FanduelProjectionsResponse = await this.fanduelService.getFanduelProjections();
+    let stats: PositionStats[] =
+      [
+        {position: 'RB'},
+        {position: 'WR'},
+        {position: 'TE'},
+        ];
+    stats.forEach(value => {
+      let fPP = fP.filter((e) => e.player.position === value.position);
+      console.log(value.position, fPP.length);
+      value.overTen = fPP.filter((e) => e.fantasy >= 10.0).length
+      value.fiveToTen = fPP.filter((e) => e.fantasy >= 5.0 && e.fantasy < 10.0).length
+      value.twoToFive = fPP.filter((e) => e.fantasy >= 2.0 && e.fantasy < 5.0).length
+      value.oneToTwo = fPP.filter((e) => e.fantasy >= 1.0 && e.fantasy < 2.0).length
+      value.underOne = fPP.filter((e) => e.fantasy > 0.0 && e.fantasy < 1.0).length
+    })
+    return stats;
   }
+}
+
+interface PositionStats {
+  position: PlayerPosition,
+  overTen?: number,
+  fiveToTen?: number,
+  twoToFive?: number
+  oneToTwo?: number,
+  underOne?: number
 }
