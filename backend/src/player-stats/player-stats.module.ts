75157cb8826c46998c1dd3139528693dc28195ff
Foxsly <timothylbelcher@gmail.com>
2026-06-09 01:13:06 -0400

    Retrieve world cup projections and stats from the FIFA service. Exclude "transferred" status because those players were left off/removed from the squads.

--- backend/src/player-stats/player-stats.module.ts
+++ backend/src/player-stats/player-stats.module.ts
@@ -1,4 +1,5 @@
 import { EventsModule } from '@/events/events.module';
+import { FifaModule } from '@/external-providers/fifa/fifa.module';
 import { FanduelModule } from '@/external-providers/fanduel/fanduel.module';
 import { SleeperModule } from '@/external-providers/sleeper/sleeper.module';
 import { Module } from '@nestjs/common';
@@ -6,7 +7,7 @@ import { PlayerStatsController } from './player-stats.controller';
 import { PlayerStatsService } from './player-stats.service';
 
 @Module({
-  imports: [SleeperModule, FanduelModule, EventsModule],
+  imports: [SleeperModule, FanduelModule, EventsModule, FifaModule],
   controllers: [PlayerStatsController],
   providers: [PlayerStatsService],
   exports: [PlayerStatsService],
